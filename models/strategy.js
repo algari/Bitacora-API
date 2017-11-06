fd1a394f5d6523e3a004fc38f6d11d72292a5925
Alexander Galvis <galcorp@gmail.com>
2017-11-06 14:04:25 -0600

    Se corrige el error en el modelo de strategy

--- models/strategy.js
+++ models/strategy.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const shema = mongoose.Schema;
 
 const StrategyShema = shema({
-    _id:String,
     strategy: {type:String, unique:true},
     username: String,
     description: String
